039ddcb65afd964622ae0eb767f1be63ffa70095
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-09-12 11:51:48 -0600

    clear button reloads the page on button press

--- fun.js
+++ fun.js
@@ -6,6 +6,11 @@ const secondNumber = []
 // bringing in the HTML
 var resultsText = document.getElementById("result")
 
+function resetResults() {
+    //  build out a function that will clear our results
+    location.reload()
+}
+
 
 // Plus Click Event
 $("#plus").on("click", function () {
@@ -55,114 +60,14 @@ $("#divide").on("click", function () {
     
     console.log(firstNumber / secondNumber)
     resultsText.textContent = "Result: " + firstNumber / secondNumber;
+
 })
 
 
 // Clear Click Event
 $("#clear").on("click", function () {
+
+    resetResults()
     console.log("Clear button pushed.....")
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// On button click, run this code
-// $("#submit-button").on("click", function () {
-
-//     // get val() of input(s)
-//     var inputValue = $("#input-box").val().trim()
-//     var inputValue2 = $("#input-box2").val().trim()
-
-//     // pushing the value of the input(s) to the arrays
-//     firstNumber.push(inputValue)
-//     secondNumber.push(inputValue2)
-
-//     // loging our math to the console
-//     console.log(firstNumber[0] / secondNumber[0])
-
-//     // console.log(firstNumber + secondNumber)
-//     resultsText.textContent = "Result: " + firstNumber + secondNumber;
-
-// })
-
-
-
-
-
-
-// functions that will handle all of the math for each button
-// function plus() {
-//     firstNumber[0] + secondNumber[0]
-//     console.log("Plus Button clicked")
-// }
-
-// function minus() {
-//     firstNumber[0] - secondNumber[0]
-//     console.log("Minus Button Clicked")
-// }
-
-// function divide() {
-//     firstNumber[0] / secondNumber[0]
-//     console.log("Divide Button clicked.....")
-// }
-
-// function multiply() {
-//     firstNumber[0] * secondNumber[0]
-//     console.log("Multiply Button Clicked")
-// }
-
-// function clear() {
-//     // set value back to 0
-//     console.log("Clear Button Clicked")
-// }
-
-
-
-
-    // switch statement
-    // switch (asdf) {
-    //     // ex: // switch (new Date().getDay())
-        
-    //     case "plus":
-    //         plus()
-    //     break;
-    
-    //     case "minus":
-    //         minus()
-    //     break;
-    
-    //     case "divide": 
-    //         divide()
-    //     break;
-    
-    //     case "multiply":
-    //         multiply()
-    //     break;
-    
-    //     case "clear":
-    //         clear()
-    //     break;
-    // }
