c761be6a84554b931141bee22e225cd92339dcd4
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-09-12 11:30:12 -0600

    All Calculations are working, next step: clear button

--- fun.js
+++ fun.js
@@ -6,24 +6,142 @@ const secondNumber = []
 // bringing in the HTML
 var resultsText = document.getElementById("result")
 
-// On button click, run this code
-$("#submit-button").on("click", function () {
 
-    // get val() of input(s)
+// Plus Click Event
+$("#plus").on("click", function () {
+    
     var inputValue = $("#input-box").val().trim()
     var inputValue2 = $("#input-box2").val().trim()
-
-    // pushing the value of the input(s) to the arrays
+    
     firstNumber.push(inputValue)
     secondNumber.push(inputValue2)
-
-    // loging our math to the console
-    console.log(firstNumber[0] / secondNumber[0])
-
-    // console.log(firstNumber + secondNumber)
-    resultsText.textContent = "Result: " + firstNumber + secondNumber;
-
     
+    resultsText.textContent = "Result: " + firstNumber + secondNumber;
+})
+
+// Minus Click Event
+$("#minus").on("click", function () {
+    
+    var inputValue = $("#input-box").val().trim()
+    var inputValue2 = $("#input-box2").val().trim()
+    
+    firstNumber.push(inputValue)
+    secondNumber.push(inputValue2)
+    
+    resultsText.textContent = "Result: " + firstNumber - secondNumber;
+})
+
+// Multiply Click Event
+$("#multiply").on("click", function () {
+    
+    var inputValue = $("#input-box").val().trim()
+    var inputValue2 = $("#input-box2").val().trim()
+    
+
+    firstNumber.push(inputValue)
+    secondNumber.push(inputValue2)
+    
+    resultsText.textContent = "Result: " + firstNumber * secondNumber;
+})
+
+// Divide Click Event
+$("#divide").on("click", function () {
+    
+    var inputValue = $("#input-box").val().trim()
+    var inputValue2 = $("#input-box2").val().trim()
+    
+    firstNumber.push(inputValue)
+    secondNumber.push(inputValue2)
+    
+    console.log(firstNumber / secondNumber)
+    resultsText.textContent = "Result: " + firstNumber / secondNumber;
+})
+
+
+// Clear Click Event
+$("#clear").on("click", function () {
+    console.log("Clear button pushed.....")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// On button click, run this code
+// $("#submit-button").on("click", function () {
+
+//     // get val() of input(s)
+//     var inputValue = $("#input-box").val().trim()
+//     var inputValue2 = $("#input-box2").val().trim()
+
+//     // pushing the value of the input(s) to the arrays
+//     firstNumber.push(inputValue)
+//     secondNumber.push(inputValue2)
+
+//     // loging our math to the console
+//     console.log(firstNumber[0] / secondNumber[0])
+
+//     // console.log(firstNumber + secondNumber)
+//     resultsText.textContent = "Result: " + firstNumber + secondNumber;
+
+// })
+
+
+
+
+
+
+// functions that will handle all of the math for each button
+// function plus() {
+//     firstNumber[0] + secondNumber[0]
+//     console.log("Plus Button clicked")
+// }
+
+// function minus() {
+//     firstNumber[0] - secondNumber[0]
+//     console.log("Minus Button Clicked")
+// }
+
+// function divide() {
+//     firstNumber[0] / secondNumber[0]
+//     console.log("Divide Button clicked.....")
+// }
+
+// function multiply() {
+//     firstNumber[0] * secondNumber[0]
+//     console.log("Multiply Button Clicked")
+// }
+
+// function clear() {
+//     // set value back to 0
+//     console.log("Clear Button Clicked")
+// }
+
+
+
+
     // switch statement
     // switch (asdf) {
     //     // ex: // switch (new Date().getDay())
@@ -48,31 +166,3 @@ $("#submit-button").on("click", function () {
     //         clear()
     //     break;
     // }
-})
-
-
-// functions that will handle all of the math for each button
-function plus() {
-    firstNumber[0] + secondNumber[0]
-    console.log("Plus Button clicked")
-}
-
-function minus() {
-    firstNumber[0] - secondNumber[0]
-    console.log("Minus Button Clicked")
-}
-
-function divide() {
-    firstNumber[0] / secondNumber[0]
-    console.log("Divide Button clicked.....")
-}
-
-function multiply() {
-    firstNumber[0] * secondNumber[0]
-    console.log("Multiply Button Clicked")
-}
-
-function clear() {
-    // set value back to 0
-    console.log("Clear Button Clicked")
-}
