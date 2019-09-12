72ffdc619cbbdc0711c0c36043ef330d032c1d91
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-09-12 13:55:48 -0600

    got nasty with the style

--- fun.js
+++ fun.js
@@ -3,6 +3,8 @@
 const firstNumber = []
 const secondNumber = []
 
+var resultsText = document.getElementById("result")
+
 function resetResults() {
     //  build out a function that will clear our results
     location.reload()
@@ -17,7 +19,13 @@ $("#plus").on("click", function () {
     firstNumber.push(inputValue)
     secondNumber.push(inputValue2)
     
-    resultsText.textContent = "Result: " + firstNumber + secondNumber;
+    resultsText.textContent = firstNumber + secondNumber;
+
+    $("#clear").show()
+    $("#plus").hide()
+    $("#minus").hide()
+    $("#multiply").hide()
+    $("#divide").hide()
 })
 
 // Minus Click Event
@@ -29,7 +37,13 @@ $("#minus").on("click", function () {
     firstNumber.push(inputValue)
     secondNumber.push(inputValue2)
     
-    resultsText.textContent = "Result: " + firstNumber - secondNumber;
+    resultsText.textContent = firstNumber - secondNumber || secondNumber - firstNumber;
+
+    $("#clear").show()
+    $("#plus").hide()
+    $("#minus").hide()
+    $("#multiply").hide()
+    $("#divide").hide()
 })
 
 // Multiply Click Event
@@ -42,7 +56,13 @@ $("#multiply").on("click", function () {
     firstNumber.push(inputValue)
     secondNumber.push(inputValue2)
     
-    resultsText.textContent = "Result: " + firstNumber * secondNumber;
+    resultsText.textContent = firstNumber * secondNumber;
+
+    $("#clear").show()
+    $("#plus").hide()
+    $("#minus").hide()
+    $("#multiply").hide()
+    $("#divide").hide()
 })
 
 // Divide Click Event
@@ -55,11 +75,78 @@ $("#divide").on("click", function () {
     secondNumber.push(inputValue2)
     
     console.log(firstNumber / secondNumber)
-    resultsText.textContent = "Result: " + firstNumber / secondNumber;
+    resultsText.textContent = firstNumber / secondNumber;
+
+    $("#clear").show()
+    $("#plus").hide()
+    $("#minus").hide()
+    $("#multiply").hide()
+    $("#divide").hide()
 })
 
 // Clear Click Event
 $("#clear").on("click", function () {
     resetResults()
 })
+
+// 
+$("#clear").hide()
+
+
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
 
